45674192fe1f73167bc7173e23d0006a4998b0a4
Adelmo Freitas <adelmo.freitas@serpro.gov.br>
2016-07-17 11:50:08 -0300

    remoção dos arquivos gerados automaticamente

--- app/inicio.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var principal_component_1 = require('./principal.component');
-platform_browser_dynamic_1.bootstrap(principal_component_1.PrincipalComponent);
-//# sourceMappingURL=inicio.js.map
